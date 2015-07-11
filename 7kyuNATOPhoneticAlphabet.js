0fd2089c48504283f29862d2e941b0fdf96d6a17
Kyle Cho <kylecho.work@gmail.com>
2015-07-10 23:27:17 -0700

    Add 7kyu NatoPhoneticAlphabet.js

---
+++ 7kyuNATOPhoneticAlphabet.js
@@ -0,0 +1,46 @@
+// My solutions
+var nato = (function() {
+  var letters =  {
+    "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
+    "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
+    "G": "Golf",   "H": "Hotel",   "I": "India",
+    "J": "Juliett","K": "Kilo",    "L": "Lima",
+    "M": "Mike",   "N": "November","O": "Oscar",
+    "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
+    "S": "Sierra", "T": "Tango",   "U": "Uniform",
+    "V": "Victor", "W": "Whiskey", "X": "X-ray",
+    "Y": "Yankee", "Z": "Zulu"
+  };
+  
+  return function(word) {
+    var result = [];
+    for (var i = 0; i < word.length; i++) {
+      var letter = word[i].toUpperCase();
+      if (letters[letter]) {
+        result.push(letters[letter]);
+      }
+    }
+    return result.join(' ');
+  };
+})()
+
+// Best practices
+var nato = (function() {
+  var letters =  {
+    "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
+    "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
+    "G": "Golf",   "H": "Hotel",   "I": "India",
+    "J": "Juliett","K": "Kilo",    "L": "Lima",
+    "M": "Mike",   "N": "November","O": "Oscar",
+    "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
+    "S": "Sierra", "T": "Tango",   "U": "Uniform",
+    "V": "Victor", "W": "Whiskey", "X": "X-ray",
+    "Y": "Yankee", "Z": "Zulu"
+  }
+  
+  return function(word) {
+    return word.split('').map(function(v) {
+      return letters[v.toUpperCase()]
+    }).join(' ');
+  }
+})()
