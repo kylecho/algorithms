04c2899b4037257051c17d36e7fbb026f072c246
Kyle Cho <kylecho.work@gmail.com>
2015-12-01 22:42:42 -0800

    Implement allAnagram

---
+++ allAnagram.js
@@ -0,0 +1,18 @@
+function allanagram (word) {
+  if (word.length < 2) {
+    return [word];
+  } else {
+    var allanswers = [];
+    for (var i = 0; i < word.length; i++) {
+      var letter = word[i];
+      var shortWord = word.substr(0, i) + word.substr(i+1, word.length - 1);
+      var shortWordArray = allanagram(shortWord);
+      for (var j = 0; j < shortWordArray.length; j++) {
+        allanswers.push(letter + shortWordArray[j]);
+      }
+    }
+    return allanswers;
+  }
+}
+
+allanagram("abc");
