e145f2d89b4ccada7f982655c62f44c68229bd8b
Kyle Cho <kylecho.work@gmail.com>
2017-06-14 22:19:16 -0700

    Save changes

--- allAnagram.js
+++ allAnagram.js
@@ -1,18 +1,18 @@
 function allanagram (word) {
   if (word.length < 2) {
-    return [word];
+    return [word]
   } else {
-    var allanswers = [];
+    var allanswers = []
     for (var i = 0; i < word.length; i++) {
-      var letter = word[i];
-      var shortWord = word.substr(0, i) + word.substr(i+1, word.length - 1);
-      var shortWordArray = allanagram(shortWord);
+      var letter = word[i]
+      var shortWord = word.substr(0, i) + word.substr(i + 1, word.length - 1)
+      var shortWordArray = allanagram(shortWord)
       for (var j = 0; j < shortWordArray.length; j++) {
-        allanswers.push(letter + shortWordArray[j]);
+        allanswers.push(letter + shortWordArray[j])
       }
     }
-    return allanswers;
+    return allanswers
   }
 }
 
-allanagram("abc");
+allanagram('abc')
